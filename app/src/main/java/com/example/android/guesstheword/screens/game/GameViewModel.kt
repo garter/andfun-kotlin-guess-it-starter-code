48667b4a83b8ff59d124aebedb8bbc372957882c
Bairam Batirov <hamraev.bairm@gmail.com>
2019-10-11 21:01:38 +0200

    Add Kotlin Project in Git

---
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -0,0 +1,91 @@
+package com.example.android.guesstheword.screens.game
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GameViewModel : ViewModel() {
+
+    private val _word = MutableLiveData<String>()
+    val word : LiveData<String>
+    get() = _word
+
+    private val _score = MutableLiveData<Int>()
+    val score : LiveData<Int>
+    get() = _score
+
+    private val _eventGameFinish =MutableLiveData<Boolean>()
+    val eventGameFinish : LiveData<Boolean>
+    get() = _eventGameFinish
+
+    // The list of words - the front of the list is the next word to guess
+    private lateinit var wordList: MutableList<String>
+
+    init {
+        Log.i("GameViewModel", "GameViewModel has created!")
+        _eventGameFinish.value = false
+        resetList()
+        nextWord()
+        _score.value = 0
+    }
+
+    /**
+     * Resets the list of words and randomizes the order
+     */
+    private fun resetList() {
+        wordList = mutableListOf(
+                "queen",
+                "hospital",
+                "basketball",
+                "cat",
+                "change",
+                "snail",
+                "soup",
+                "calendar",
+                "sad",
+                "desk",
+                "guitar",
+                "home",
+                "railway",
+                "zebra",
+                "jelly",
+                "car",
+                "crow",
+                "trade",
+                "bag",
+                "roll",
+                "bubble"
+        )
+        wordList.shuffle()
+    }
+
+    /**
+     * Moves to the next word in the list
+     */
+    private fun nextWord() {
+        //Select and remove a word from the list
+        if (wordList.isEmpty()) {
+            _eventGameFinish.value = true
+        } else {
+            _word.value = wordList.removeAt(0)
+        }
+
+    }
+
+    fun onGameFinishComplate(){
+        _eventGameFinish.value = false
+    }
+
+    /** Methods for buttons presses **/
+
+   fun onSkip() {
+        _score.value = (score.value)?.minus(1)
+        nextWord()
+    }
+
+    fun onCorrect() {
+        _score.value = (score.value)?.plus(1)
+        nextWord()
+    }
+}
